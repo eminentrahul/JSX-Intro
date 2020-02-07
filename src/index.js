0aec0c9ba506ce66ec5759359a38637a07a06b1d
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2020-02-07 22:32:31 +0530

    Added Image.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,26 @@ import ReactDOM from "react-dom";
 const name = "Rahul Ravi Prakash";
 const profession = "iOS Developer";
 const currentYear = new Date().getFullYear();
+
+const img = "https://picsum.photos/200";
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
+    <h1 className="heading">My Favourite Foods</h1>
     <ul>
-      <li>Chicken Popcorn</li>
+      {/* <li>Chicken Popcorn</li>
       <li>Chicken Pakoda</li>
-      <li>Chicken Reshmi kebab</li>
+      <li>Chicken Reshmi kebab</li> */}
+      <img alt="MyTickPhoto" src={img} /> /Added Image
     </ul>
-    <div>
+
+    {/* <div>
       <h2>My name is {name}</h2>
       <p>I'm an {profession}</p>
     </div>
     <div>
       <p>Created by {name} </p>
       <p>Copyright {currentYear}</p>
-    </div>
+    </div> */}
   </div>,
 
   document.getElementById("root")
