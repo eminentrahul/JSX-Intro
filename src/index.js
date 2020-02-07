4fa9683dd157c2981b1f66462dfb29da0be27595
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2020-02-07 21:38:57 +0530

    Added List

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph</p>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Chicken Popcorn</li>
+      <li>Chicken Pakoda</li>
+      <li>Chicken Reshmi kebab</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
