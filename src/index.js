3a66f9b2a3266f95081edae2276a54a5b9f7c459
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2020-02-07 22:09:51 +0530

    Added JS Expression

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Rahul Ravi Prakash";
+const profession = "iOS Developer";
+const currentYear = new Date().getFullYear();
 ReactDOM.render(
   <div>
     <h1>My Favourite Foods</h1>
@@ -9,6 +12,15 @@ ReactDOM.render(
       <li>Chicken Pakoda</li>
       <li>Chicken Reshmi kebab</li>
     </ul>
+    <div>
+      <h2>My name is {name}</h2>
+      <p>I'm an {profession}</p>
+    </div>
+    <div>
+      <p>Created by {name} </p>
+      <p>Copyright {currentYear}</p>
+    </div>
   </div>,
+
   document.getElementById("root")
 );
