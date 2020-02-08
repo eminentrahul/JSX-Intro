7d4cd335e796e1eccc968ba9bab00da4db0438ba
Rahul Ravi Prakash <eminentrahul@rocketmail.com>
2020-02-08 21:05:10 +0530

    Component Practice

---
+++ src/Components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Chicken Tikka</li>
+      <li>Chicken Tandoori</li>
+      <li>Chicken Kebab</li>
+    </ul>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const currentTime = new Date(2020, 1, 1, 16).getHours();
-
-const customColor = {
-  color: ""
-};
-
-var greeting;
-
-if (currentTime < 12) {
-  greeting = "Good Morning!";
-  customColor.color = "red";
-} else if (currentTime < 18) {
-  greeting = "Good Evening!";
-  customColor.color = "Blue";
-} else {
-  greeting = "Good Night!";
-  customColor.color = "Green";
-}
+import App from "./Components/App";
 
 ReactDOM.render(
-  <div>
-    <h1 className="heading">{greeting}</h1>
-  </div>,
+  <App />,
 
   document.getElementById("root")
 );
